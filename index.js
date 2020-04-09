8bdb95fff00668914867d5c6494753a2e2d24d5e
fauzi <M.fauzi_87@hotmail.com>
2020-04-09 20:47:25 +0800

    improve code

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-let reverseNum = num => {
-  if (typeof num !== "number") {
-    return "please provide a integer";
+let reverseNum = (num) => {
+  if (typeof num !== 'number') {
+    return 'please provide a integer'
   }
-  let arrStr = num.toString().split("");
-  if (arrStr[0] === "-") {
-    arrStr.shift();
-    let arrChar = ignoreZero(arrStr.reverse());
-    arrChar.unshift("-");
-    return Number(arrChar.join(""));
+  let arrStr = num.toString().split('')
+  if (arrStr[0] === '-') {
+    arrStr.shift()
+    arrStr.reverse()
+    arrStr.unshift('-')
+    return Number(arrStr.join(''))
   }
-  return Number(arrStr.reverse().join(""));
-};
+  return Number(arrStr.reverse().join(''));
+}
 
