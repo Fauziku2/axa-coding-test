961a9af916043afbf67e4bb6f6a3beb9f4a0f3b7
Fauzi Mohd Ghouse <m.fauzi_87@hotmail.com>
2020-04-13 10:42:13 +0800

    added more test

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-let reverseNum = (num) => {
-  if (typeof num !== 'number') {
-    return 'please provide a integer'
+let reverseNum = num => {
+  if (typeof num !== "number") {
+    return "please provide a integer";
   }
-  let arrStr = num.toString().split('')
-  if (arrStr[0] === '-') {
-    arrStr.shift()
-    arrStr.reverse()
-    arrStr.unshift('-')
-    return Number(arrStr.join(''))
+  let arrStr = num.toString().split("");
+  if (arrStr[0] === "-") {
+    arrStr.shift();
+    arrStr.reverse();
+    arrStr.unshift("-");
+    return Number(arrStr.join(""));
   }
-  return Number(arrStr.reverse().join(''));
-}
-
+  return Number(arrStr.reverse().join(""));
+};
--- indexSpace.js
+++ indexSpace.js
@@ -1,6 +1,14 @@
 describe("interview test", function() {
-  it("expect reverseNum to return reverse integer", function() {
+  it("expect reverseNum(-115) to return -511", function() {
     expect(reverseNum(-115)).toBe(-511);
+  });
+
+  it("expect reverseNum(-50) to return -5", function() {
+    expect(reverseNum(-50)).toBe(-5);
+  });
+
+  it("expect reverseNum(400) to return 4", function() {
+    expect(reverseNum(400)).toBe(4);
   });
 
   it("expect reverseNum with parameter string to return err", function() {
